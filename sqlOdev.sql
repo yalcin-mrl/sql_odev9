14defc78e9eb08255dbcb46da484dbdd33b68324
yalcin-mrl <94745103+yalcin-mrl@users.noreply.github.com>
2022-06-23 15:02:37 +0300

    initial commit

---
+++ sqlOdev.sql
@@ -0,0 +1,3 @@
+SELECT city,country FROM city INNER JOIN country on city.country_id = country.country_id;
+SELECT first_name, last_name,payment.payment_id FROM customer INNER JOIN payment ON payment.customer_id =customer.customer_id;
+SELECT first_name,last_name,rental.rental_id FROM customer INNER JOIN rental ON rental.customer_id = customer.customer_id;
